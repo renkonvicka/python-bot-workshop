9fbeb33e875c8b37e98e6280d50572f89df97c28
Ren Konvicka <Kahleesi@Khaleesi.local>
2020-05-11 22:19:37 -0500

    initial commit

---
+++ discord_bots/godbotogcode.py
@@ -0,0 +1,58 @@
+#initializing
+print("Greetings, Mortals and Ren. I am the deity Godbot, and I have taken an interest in your worldly goings-on. If you would like to speak, simply ask for me.")
+
+
+
+looping = True
+
+#initiating point counter
+
+#write a neat little function that says that for every message a user sends, a user gains one point.
+
+#calling godbot
+#responses
+
+while looping:
+
+    call = input()
+
+    if call.upper() == "GODBOT?":
+        print("I have been summoned.")
+        looping == True
+
+    elif call.upper() == "HEY":
+        print("Hi!")
+        looping == True
+
+#commands
+    #does a google search and returns first 3 results in links
+#elif call.upper() == "GODBOT? SEARCH":
+
+    #Opens chat search and searches for keywords or phrases
+#elif call.upper() == "GODBOT? SEARCH CHAT FOR":
+
+    #smiting of a user
+#elif call.upper() == "GODBOT? SMITE @USER":
+        #restart point counter for user
+        #print("User has been smote. Point counter return to 0.")
+
+    #shaming of a user
+#elif call.upper() == "GODBOT? SHAME @USER":
+        #print("User has been shamed. Point counter not effected. Shame is punishment enough.")
+
+    #display point counter
+#elif call.upper() == "GODBOT? DISPLAY POINTS.":
+        #return points of everyone in server like so: @user has x points. @nextuser has y points. etc
+
+#elif call.upper() == "GODBOT? SHOW ME STR":
+        #returns first 3 images for keywords and phrases
+
+else:
+    looping == True
+
+#damning
+    #if a user says a certain thing, they will be damned.
+
+
+#blessing
+    #if a user says a certain thing, they will be blessed.
